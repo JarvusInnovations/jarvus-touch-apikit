aae983fc02b2ae031a95217942cc2f049e9f3dc7
Chris Alfano <chris@jarv.us>
2014-10-23 19:41:56 -0400

    added singleton for proxy default

---
+++ src/util/API.js
@@ -0,0 +1,10 @@
+/* jshint undef: true, unused: true, browser: true, quotmark: single, curly: true */
+/* global Ext */
+
+/**
+ * A default singleton instance for {@link Jarvus.touch.util.AbstractAPI}
+ */
+Ext.define('Jarvus.touch.util.API', {
+    extend: 'Jarvus.touch.util.AbstractAPI',
+    singleton: true
+});
